9cd1ad115323c84381aeadc129b085d6156246e9
Daniel Kim <kkdh1109@gmail.com>
2021-04-25 19:57:35 -0400

    declare global variables, create a particle blueprint, and functions to randomly generate coorinates for particles and draw them

--- script.js
+++ script.js
@@ -5,3 +5,61 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 let particleArray = [];
+
+let mouse = {
+    x: null,
+    y: null,
+    radius: 150
+}
+
+window.addEventListener('mousemove', function (e) {
+    mouse.x = event.x;
+    mouse.y = event.y;
+});
+
+ctx.fillStyle = 'white';
+ctx.font = '30px Verdana';
+//2nd = x, 3rd = y coordinate, 4th = optional, can define max width
+ctx.fillText('A', 0, 40);
+const data = ctx.getImageData(0, 0, 100, 100);
+
+class Particle {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.size = 3;
+        this.baseX = this.x;
+        this.baseY = this, y;
+        this.density = (Math.random() * 30) + 1;
+    }
+
+    draw() {
+        ctx.fillStyle = 'white';
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+        ctx.closePath();
+        ctx.fill();
+    }
+}
+
+function init() {
+    particleArray = [];
+    for (let i = 0; i < 10; i++) {
+        let x = Math.random() * canvas.width;
+        let y = Math.random() * canvas.height;
+        particleArray.push(new Particle(x, y))
+    }
+}
+
+init();
+console.log(particleArray)
+
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    for (let i = 0; i < particleArray.length; i++) {
+        particleArray[i].draw();
+    }
+    requestAnimationFrame(animate);
+}
+
+animate();
