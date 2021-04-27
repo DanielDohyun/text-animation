de9f0408b23199fea30b77116318a6758ea73286
Daniel Kim <kkdh1109@gmail.com>
2021-04-26 21:21:35 -0400

    add line that connects adjacent particle

--- script.js
+++ script.js
@@ -5,11 +5,13 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 let particleArray = [];
+let adjustX = 6;
+let adjustY = 0;
 
 let mouse = {
     x: null,
     y: null,
-    radius: 250
+    radius: 180
 }
 
 window.addEventListener('mousemove', function (e) {
@@ -18,10 +20,11 @@ window.addEventListener('mousemove', function (e) {
 });
 
 ctx.fillStyle = 'white';
-ctx.font = '30px Verdana';
+ctx.font = '28px Verdana';
 //2nd = x, 3rd = y coordinate, 4th = optional, can define max width
-ctx.fillText('A', 0, 40);
-const data = ctx.getImageData(0, 0, 100, 100);
+ctx.fillText('Text', 30, 40);
+ctx.fillText('Animation', 0, 75);
+const textCoordinates = ctx.getImageData(0, 0, 140, 140);
 
 class Particle {
     constructor(x, y) {
@@ -34,7 +37,7 @@ class Particle {
     }
 
     draw() {
-        ctx.fillStyle = 'red';
+        ctx.fillStyle = 'white';
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
         ctx.closePath();
@@ -70,17 +73,25 @@ class Particle {
     }
 }
 
+console.log(textCoordinates)
 function init() {
     particleArray = [];
-    for (let i = 0; i < 500; i++) {
-        let x = Math.random() * canvas.width;
-        let y = Math.random() * canvas.height;
-        particleArray.push(new Particle(x, y))
+
+    for (let y = 0, y2 = textCoordinates.height; y < y2; y++) {
+        for (let x = 0, x2 = textCoordinates.width; x < x2; x++) {
+
+            //data array is a clampedArray (contain only numbers btw 0-255)
+            //any pixel with more than 50% opacity will be added to particleArr
+            if (textCoordinates.data[(y * 4 * textCoordinates.width) + (x * 4) + 3] > 128) {
+                let positionX = x + adjustX;
+                let positionY = y + adjustY;
+                particleArray.push(new Particle(positionX * 8, positionY * 10));
+            }
+        }
     }
 }
 
 init();
-console.log(particleArray)
 
 function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -88,7 +99,29 @@ function animate() {
         particleArray[i].draw();
         particleArray[i].update();
     }
+    connect();
     requestAnimationFrame(animate);
 }
 
 animate();
+
+function connect() {
+    let opacityValue = 1;
+    for (let a = 0; a < particleArray.length; a++) {
+        for (let b = a; b < particleArray.length; b++) {
+            let dx = particleArray[a].x - particleArray[b].x;
+            let dy = particleArray[a].y - particleArray[b].y;
+            let distance = Math.sqrt(dx * dx + dy * dy);
+
+            if (distance < 30) {
+                opacityValue = 1 - (distance / 50);
+                ctx.strokeStyle = 'rgba(255, 255, 255,' + opacityValue + ')';
+                ctx.lineWidth = 2;
+                ctx.beginPath();
+                ctx.moveTo(particleArray[a].x, particleArray[a].y);
+                ctx.lineTo(particleArray[b].x, particleArray[b].y);
+                ctx.stroke();
+            }
+        }
+    }
+}
